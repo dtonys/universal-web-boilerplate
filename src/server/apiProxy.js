778201aef9e101b5b1a363882a55247e06574af7
dtonys@gmail.com <dtonys@gmail.com>
2018-01-26 20:15:29 -0800

    changeOrigin option required for proxy-ing across domains

--- src/server/apiProxy.js
+++ src/server/apiProxy.js
@@ -4,8 +4,16 @@ import httpProxy from 'http-proxy';
 function createProxy( target ) {
   const proxy = httpProxy.createProxyServer();
 
+  // proxy.on('proxyRes', function (proxyRes, req, res) {
+  //   console.log('RAW Headers from the target', JSON.stringify(proxyRes.headers, true, 2));
+  //   console.log('RAW Body from the target', JSON.stringify(proxyRes.body, true, 2));
+  // });
+
   return (req, res, next) => {
-    proxy.web(req, res, { target }, ( error ) => {
+    proxy.web(req, res, {
+      target,
+      changeOrigin: true,
+    }, ( error ) => {
       next(error);
     });
   };
